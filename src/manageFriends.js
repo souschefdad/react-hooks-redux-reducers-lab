64b8fc0db6c5c7c097ea60d3240180deffa62c41
Pierce <piercedavidraphael@gmail.com>
2022-01-11 13:24:18 -0600

    commit tree d62355b1500808bb8fcb6b02874d02fd3b318383

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,22 @@
 export function manageFriends(state, action) {
-  // your code here
+  state = {friends: []}
+  action = {
+    type: "friends/add",
+    payload: {
+      name: "Chrome Boi",
+      homewtown: "NYC",
+      id: 1
+    }
+  }
+  switch (action.type) {
+    case "friends/add":
+      return { friends: [...state.friends, action.payload], 
+      };
+    case "friends/remove":
+      return { 
+        friends: state.friends.filter((friend) => friend.id !== action.payload),
+      };
+    default:
+      return state;
+  }
 }
